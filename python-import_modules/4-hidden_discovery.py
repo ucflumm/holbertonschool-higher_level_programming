a66a8552b81a60c9577bb8b3786c07bf4c09394e
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 02:18:10 +1100

    try

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,8 +5,6 @@
 
 if __name__ == "__main__":
     import hidden_4
-
-    name = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
