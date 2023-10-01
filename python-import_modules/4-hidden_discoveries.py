85c2c8b6154e1aa0b88c7aa39a2a78e368ac05b8
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 02:15:39 +1100

    try

--- python-import_modules/4-hidden_discoveries.py
+++ python-import_modules/4-hidden_discoveries.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
-# 
+# Write a program that purintz all the names defined by the compiled module
+# CENSORED_FILE.
+
+if __name__ == "__main__":
+    import hidden_4
+
+    name = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
