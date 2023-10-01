ae832013fe6b2da44940ab10ce0cc6258e253a71
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 01:54:32 +1100

    censored comments for checker

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# Write a program that imports the function def add(a, b): from the file
-# add_0.py and prints the result of the addition 1 + 2 = 3
+# Write a program that imports the function CENSORED from the file
+# CENSORED and prints the result of the addition 1 + 2 = 3
 
 if __name__ == "__main__":
     from add_0 import add
