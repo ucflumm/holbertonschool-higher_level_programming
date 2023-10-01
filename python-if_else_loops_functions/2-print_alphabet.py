34aae686da813b5de6135a0ec6a023c928757782
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 00:37:06 +1100

    file 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 # Write a program that prints the ASCII alphabet,
 # in lowercase, not followed by a new line.
 
 for i in range(97, 123):
     if i != 113 and i != 101:
-        print(chr(i), end='')
+        print("{:c}".format(i), end="")
