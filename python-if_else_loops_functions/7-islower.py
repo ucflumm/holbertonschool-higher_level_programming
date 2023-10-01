625f9046d58d4d7eb064624ab449a81e7594ea57
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 01:00:04 +1100

    pcs comp

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
-# 
+# Write a function that checks for lowercase character.
+# Prototype: def islower(c):
+# Returns True if c is lowercase
+# Returns False otherwise
+# You are not allowed to import any module
+# You are not allowed to use str.upper() and str.isupper()
+
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
