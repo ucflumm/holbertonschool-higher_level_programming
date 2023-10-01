f2d40f824c9cb3f5fc0c89e3f07e79e5a3d72746
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 01:52:24 +1100

    pcs comp

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
-# 
+# Write a program that imports the variable a from the file variable_load_5.py
+# and prints its value.
+
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
