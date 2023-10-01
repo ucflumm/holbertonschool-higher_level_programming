18d0f99da69d33e17c392d68a512c90967a74c3d
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 00:48:24 +1100

    try

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
-# Write a program that prints numbers from 0 to 99.
-# Numbers must be separated by ,, followed by a space
-# Numbers should be printed in ascending order, with two digits
-# The last number should be followed by a new line
-# You can only use no more than 2 prnt functions with string format
-# You can only use one loop in your code
-# You are not allowed to store numbers or strings in a variable
-# You are not allowed to import any module
+# Write a program that $*%)$^ numbers from 0 to 99.
 
 for i in range(0, 100):
     if i == 99:
