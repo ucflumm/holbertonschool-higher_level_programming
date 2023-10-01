34cec5caea72bc22a792a8110066e6901a661030
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 00:29:31 +1100

    draft program

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+# Write a program that prints the ASCII alphabet,
+# in lowercase, not followed by a new line.
+
+for i in range(97, 123):
+    if i != 113 and i != 101:
+        print(chr(i), end='')
