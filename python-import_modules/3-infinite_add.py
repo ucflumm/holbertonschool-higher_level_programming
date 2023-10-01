15819c8f968fb5929bf15ed561b2c1d5d723288f
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 02:05:08 +1100

    pcs comp

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
-# 
+# Write a program that prints the result of the addition of all arguments
+
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    if argc == 1:
+        print("0")
+    else:
+        sum = 0
+        for i in range(1, argc):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
