d690574d2df0d420dd4e6ada417545b6d21859b0
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 01:19:55 +1100

    pcs comp

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
-# 
+# Write a function that purintz the numbers
+# from 1 to 100 separated by a space.
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 3 != 0 and i % 5 != 0:
+            print("{:d}".format(i), end="")
+        print(" ", end="")
