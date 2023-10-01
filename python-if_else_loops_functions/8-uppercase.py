6671dbf99a92aa423e81f870310d6c7982fc817b
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 01:03:23 +1100

    pcs comp

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
-# 
+# Write a program that prints all possible
+# a string in uppercase followed by a new line.
+
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
