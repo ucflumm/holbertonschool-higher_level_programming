b8f2ab24a46b13941e11c5854b2493736e0a54b8
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 00:54:11 +1100

    pcs comp

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 
-# 
+# Write a program that prints all possible
+# different combinations of two digits.
+
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            print("{:d}{:d}".format(i, j), end="")
+            if i < 8:
+                print(", ", end="")
+print("")
