1ea850475f8d9a91899e6a8923e95d04cbb035d5
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 00:24:10 +1100

    minor negative logic fix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    number = number * -1
-    numberLastDigit = number % 10
-    number = number * -1
+    last_digit = number % -10
 else:
-    numberLastDigit = number % 10
-if numberLastDigit > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, numberLastDigit))
-elif numberLastDigit == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, numberLastDigit))
+    last_digit = number % 10
+if last_digit > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
+elif last_digit == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, numberLastDigit))
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
