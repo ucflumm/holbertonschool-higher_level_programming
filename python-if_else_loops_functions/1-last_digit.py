90fda7edc1f4e815027f947b979d6f94c33dcf81
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 00:19:27 +1100

    fixed negative logic

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-numberLastDigit = number % 10
-if numberLastDigit < 0:
-    print(f'Last digit of {number} is {numberLastDigit} and is less than 6 and not 0')
-elif number == 0:
-    print(f'Last digit of {number} is {numberLastDigit} and is 0')
-elif number > 0:
-    print(f'Last digit of {number} is {numberLastDigit} and is greater than 5')
+if number < 0:
+    number = number * -1
+    numberLastDigit = number % 10
+    number = number * -1
+else:
+    numberLastDigit = number % 10
+if numberLastDigit > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, numberLastDigit))
+elif numberLastDigit == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, numberLastDigit))
+else:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, numberLastDigit))
