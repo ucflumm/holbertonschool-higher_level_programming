29d6f291e78331d26a642ae54f3ca442e5e5f176
Uwe Flumm <ucflumm@gmail.com>
2023-10-02 14:43:16 +1100

    trying different logic for checker

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
-        return None
-    else:
+    if idx > -1 and idx < len(my_list):
         return my_list[idx]
+    else:
+        return None
