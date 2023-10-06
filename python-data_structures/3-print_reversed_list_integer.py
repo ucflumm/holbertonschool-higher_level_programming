0dab90f11d5e8fa79cba3d9649f02dd538c1edc9
Uwe Flumm <ucflumm@gmail.com>
2023-10-06 13:24:20 +1100

    try

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
+    if my_list == []:
         return None
     reversed_list = reversed(my_list)
     for num in reversed_list:
