06c95bab8a1859a3af009f6061f3930fb4fc7ad0
Uwe Flumm <ucflumm@gmail.com>
2023-10-06 12:49:02 +1100

    pcs comp

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,17 +1,8 @@
 def divisible_by_2(my_list=[]):
-    try:
-        new_list = []
-        for i in range(len(my_list)):
-            if my_list[i] % 2 == 0:
-                new_list.append(True)
-            else:
-                new_list.append(False)
-        
-        if len(new_list) != len(my_list):
-            raise ValueError("Error: New list length is different from the old list length")
-
-        return new_list
-    except ZeroDivisionError:
-        return "Error: Division by zero is not allowed"
-    except ValueError as e:
-        return str(e)
+    new_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
