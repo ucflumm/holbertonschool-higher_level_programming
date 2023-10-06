bae9310db806297a6d1ce0d6897692ff85e640f2
Uwe Flumm <ucflumm@gmail.com>
2023-10-06 13:28:44 +1100

    try

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list == None:
         return None
-    reversed_list = reversed(my_list)
+    my_list.reverse()
     for num in reversed_list:
         print("{:d}".format(num))
