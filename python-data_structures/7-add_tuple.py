a4932f542578e5b0fb9ca71df3f66087a0ffddad
Uwe Flumm <ucflumm@gmail.com>
2023-10-06 11:55:10 +1100

    TRY

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0, 0) # adding 0 incase there is no value in the tupl
-    tuple_b = tuple_b + (0, 0)
-    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return tuple_c
+    # Ensure that both tuples have at least 2 elements by
+    # padding with zeros if needed
+    while len(tuple_a) < 2:
+        tuple_a += (0,)
+    while len(tuple_b) < 2:
+        tuple_b += (0,)
+
+    # Perform element-wise addition and return the result as a tuple
+    result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result_tuple
