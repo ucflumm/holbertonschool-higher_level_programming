4231427d814dd4193fb557efc49fb83204e1a162
Uwe Flumm <ucflumm@gmail.com>
2023-10-07 21:48:50 +1100

    added empty file structure

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        return None
-    my_list.reverse()
-    for num in my_list:
+    reversed_list = reversed(my_list)
+    for num in reversed_list:
         print("{:d}".format(num))
