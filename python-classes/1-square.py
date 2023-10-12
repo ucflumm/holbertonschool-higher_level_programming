e86d97dd0466c0b761687454c13843ec6f73b5c7
Uwe Flumm <ucflumm@gmail.com>
2023-10-12 13:10:27 +1100

    added file 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Definition of class Square"""
+
+
+class Square:
+    """Class square with attribute size"""
+    __size = 0
+
+    def __init__(self, size):
+        """ Init square variable size """
+        self.__size = size
