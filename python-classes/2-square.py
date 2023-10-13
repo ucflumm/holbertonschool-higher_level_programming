ff4507ceebf1e9e0ce7c8acd444a9030684fddaf
Uwe Flumm <ucflumm@gmail.com>
2023-10-13 11:29:44 +1100

    added changes

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Definition of class Square"""
+
+
+class Square:
+    """Class square with attribute size"""
+
+    def __init__(self, size=0):
+        """ Initialize the square with a given size """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
