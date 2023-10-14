c3a062e07acb20db380b802e3ff1466046fa4d4a
Uwe Flumm <ucflumm@gmail.com>
2023-10-14 22:44:37 +1100

    pcs comp

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -49,3 +49,10 @@ class Rectangle:
             return 0
         else:
             return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """return string of rectangle made up of #"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return (("#" * self.__width + "\n") * self.__height)[:-1]
