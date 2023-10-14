078f67de72b949bcd2cb6e9cf57c28d4094ed7d9
Uwe Flumm <ucflumm@gmail.com>
2023-10-14 21:05:53 +1100

    pcs comp

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,3 +14,32 @@ class Square:
             raise ValueError("size must be >=0")
         else:
             self.__size = size
+
+    def area(self):
+        """ Return the area of the square """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ Return the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Set the size of the square """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """ Print the square """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
