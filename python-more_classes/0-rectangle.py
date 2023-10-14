141a0271f74a2b61599ad971dbf57469565a5eb5
Uwe Flumm <ucflumm@gmail.com>
2023-10-14 22:20:55 +1100

    pcs comp

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -4,7 +4,3 @@
 
 class Rectangle:
     """Class rectangle with private attributes width and height"""
-    def __init__(self, width=0, height=0):
-        """assign each arguments width and height to each attribute"""
-        self.width = width
-        self.height = height
--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Module 0-rectangle.py"""
+
+
+class Rectangle:
+    """Class rectangle with private attributes width and height"""
+    def __init__(self, width=0, height=0):
+        """assign each arguments width and height to each attribute"""
+        self.width = width
+        self.height = height
