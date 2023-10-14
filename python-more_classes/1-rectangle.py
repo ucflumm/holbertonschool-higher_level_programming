368fbd5ff76bf5c6ff60443ff6d276e731e8dc02
Uwe Flumm <ucflumm@gmail.com>
2023-10-14 22:32:35 +1100

    moved sanity checks to setter method

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,26 +6,8 @@ class Rectangle:
     """Class rectangle with private attributes width and height"""
     def __init__(self, width=0, height=0):
         """assign each arguments width and height to each attribute"""
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
         self.width = width
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
         self.height = height
-
-    @property
-    def width(self):
-        """return value of width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """set value of width"""
-        self.__width = value
 
     @property
     def height(self):
@@ -35,4 +17,24 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set value of height"""
-        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise TypeError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """return value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set value of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
+        else:
+            self.__width = value
