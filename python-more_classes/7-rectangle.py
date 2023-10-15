943bdcdb142d337e697ed4e52048ace9314ea664
Uwe Flumm <ucflumm@gmail.com>
2023-10-15 13:56:16 +1100

    pcs comp

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -59,7 +59,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            return ((Rectangle.print_symbol * self.__width + "\n") * self.__height)[:-1]
+            return ((str(self.print_symbol) * self.__width + "\n")
+                    * self.__height)[:-1]
 
     def __repr__(self):
         """return string representation of rectangle"""
