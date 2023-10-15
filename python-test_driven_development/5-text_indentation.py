f9d820f9cfdaa700532b5302f52a8d82f745ca57
Uwe Flumm <ucflumm@gmail.com>
2023-10-15 22:09:35 +1100

    readded bash statementr

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Module for 5-text_indentation.py"""
+
+
 def text_indentation(text):
     """Function that prints a text with 2 new lines after each of these
     characters: ., ?, and :"""
