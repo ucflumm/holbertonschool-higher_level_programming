42932c26f8d40b06d3890910361e95d0cf86da5b
Uwe Flumm <ucflumm@gmail.com>
2023-10-15 16:04:16 +1100

    added code for question 4 and 5

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""module 4-print_square.py"""
+
+
+def print_square(size):
+    """function that prints a square with the character #"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise TypeError("size must be >= 0")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print()
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Module 5-text_indentation.py"""
+
+
+def text_indentation(text):
+    """Function that prints a text with 2 new lines after each of these
+    characters: ., ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    else:
+        for i in range(len(text)):
+            if text[i] == '.' or text[i] == '?' or text[i] == ':':
+                print(text[i])
+                print()
+            else:
+                print(text[i], end="")
