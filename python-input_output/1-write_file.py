3100c0ddadbd3f5c954b87540b0a71385a636607
Uwe Flumm <ucflumm@gmail.com>
2023-10-20 22:20:17 +1100

    pcs comp

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+    Module: write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+        Writes a string to a text file (UTF8) and returns the number of
+        characters written
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return file.write(text)
