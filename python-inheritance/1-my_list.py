aa727df55c8efb6781b1d3796dce2866acfa2541
Uwe Flumm <ucflumm@gmail.com>
2023-10-20 13:23:19 +1100

    pcs comp

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""My list module my_list"""
+
+
+class MyList(list):
+    """Class Mylist"""
+    def print_sorted(self):
+        "Prints list sorted"
+        print(sorted(self))
