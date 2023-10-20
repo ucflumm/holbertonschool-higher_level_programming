73baf46c957a20830fb67b51adab5cab86238af0
Uwe Flumm <ucflumm@gmail.com>
2023-10-20 22:25:08 +1100

    pcs comp

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+    module : append_write
+"""
+
+
+def append_write(filename="", text=""):
+    """
+        appends a string at the end of a text file (UTF8) and returns the
+        number of characters added
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
