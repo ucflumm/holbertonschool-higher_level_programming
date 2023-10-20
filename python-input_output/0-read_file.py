e3ee3d4f3963b9c6c2d6bfe0379413a6502947f5
Uwe Flumm <ucflumm@gmail.com>
2023-10-20 22:13:50 +1100

    pcs comp

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+    Read file module
+"""
+
+
+def read_file(filename=""):
+    """
+        Reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
