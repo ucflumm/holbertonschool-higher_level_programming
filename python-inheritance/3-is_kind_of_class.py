db3e8ae964762bb048c838bbfe447d5428217caf
Uwe Flumm <ucflumm@gmail.com>
2023-10-20 14:04:42 +1100

    pcs comp

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Module is kind of class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ func is_kind_of_class """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
