fd0580d21027dba7658a7c80be221a027620fc68
Uwe Flumm <ucflumm@gmail.com>
2023-10-20 14:00:30 +1100

    pcscomp

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" module 2-is_same_class.py """
+
+
+def is_same_class(obj, a_class):
+    """ function is same class """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
