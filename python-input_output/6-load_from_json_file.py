7a58f7f51cb99073a163710ea081368a302eda2e
Uwe Flumm <ucflumm@gmail.com>
2023-10-21 13:16:08 +1100

    pcs comp

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+    Module: load_from_json_file
+    """
+import json
+
+
+def load_from_json_file(filename):
+    """
+        creates an Object from a “JSON file”
+    """
+    # Open the file name mode read and encoding utf-8 and load the object
+    with open(filename, mode='r', encoding='utf-8') as f:
+        return json.load(f)
