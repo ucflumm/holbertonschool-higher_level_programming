7c98e22d59795153f33a35cc8c4fbfd25f65e471
Uwe Flumm <ucflumm@gmail.com>
2023-10-21 13:12:24 +1100

    pcs comp

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+    Module: save_to_json_file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        writes an Object to a text file, using a JSON representation
+    """
+    # Open the file name mode write and encoding utf-8 and dump the object
+    with open(filename, mode='w', encoding='utf-8') as f:
+        # Dump the object args my_obj to the file f
+        json.dump(my_obj, f)
