403c6541ea8c7a14d264d4959e2cea6057f4063a
Uwe Flumm <ucflumm@gmail.com>
2023-10-21 13:04:42 +1100

    pcs comp

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+    Module: from_json_string.
+"""
+
+
+def from_json_string(my_str):
+    """
+        returns an object represented by a JSON string
+    """
+    import json
+    return json.loads(my_str)
