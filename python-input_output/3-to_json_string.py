1006ec00dab4aafa9eddf4f19df0ba98b0b791da
Uwe Flumm <ucflumm@gmail.com>
2023-10-21 13:02:57 +1100

    pcs comp

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+    Module: to_json_string
+"""
+
+
+def to_json_string(my_obj):
+    """
+        returns the JSON representation of an object (string)
+    """
+    import json
+    return json.dumps(my_obj)
