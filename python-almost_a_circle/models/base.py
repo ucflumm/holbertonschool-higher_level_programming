419ed5f8486628b7503fdea2c14dec26ae5606a8
Uwe Flumm <ucflumm@gmail.com>
2023-10-24 13:18:03 +1100

    added base model

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""This module contains the BaseModel class"""
+
+
+class Base:
+    """
+        Base class for all other classes in this project
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+            Initialize the base class
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
