6e428d461f5d3bf5a53bad6b5dbd6caff94e6719
Uwe Flumm <ucflumm@gmail.com>
2023-10-26 11:34:15 +1100

    changed error statement slightly

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -55,7 +55,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be => 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -69,5 +69,5 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be => 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
