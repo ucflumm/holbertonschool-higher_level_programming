884c7c75ea5356214acc322e632ae5ac17084b91
Uwe Flumm <ucflumm@gmail.com>
2023-10-27 11:49:33 +1100

    added to_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,3 +17,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+            Return the JSON string representation of list_dictionaries
+        """
+        import json
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
