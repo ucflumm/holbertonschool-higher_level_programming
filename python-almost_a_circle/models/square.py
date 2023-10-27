fc6f979e95b9c765feb988380bd3b89a82fe0701
Uwe Flumm <ucflumm@gmail.com>
2023-10-27 11:44:19 +1100

    modified to_dictionary order

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -42,4 +42,4 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """ Return a dictionary representation of the square """
-        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+        return {"id": self.id, "x": self.x, 'size': self.size, "y": self.y}
