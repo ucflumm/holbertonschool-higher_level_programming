251349e9632eaaebfbd67f2992e34c9c4d9c9794
Uwe Flumm <ucflumm@gmail.com>
2023-11-23 11:47:28 +1100

    Add second_table and insert data

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,1 +1,14 @@
 -- File 9
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT NOT NULL,
+    `name` VARCHAR(256) NOT NULL,
+    `score` INT NOT NULL,
+    PRIMARY KEY (`id`)
+);
+INSERT INTO `second_table`
+(`id`, `name`, `score`)
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
