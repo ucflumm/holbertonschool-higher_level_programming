d4ebe4b43008e37cb71c9b0c275b4a492f985258
Uwe Flumm <ucflumm@gmail.com>
2023-11-23 11:21:12 +1100

    Add SQL files from 10.sql to 16.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Some comment here
+-- It shows all the databases
+
+CREATE TABLE IF NOT EXISTS `first_table` (
+    `id` INT NOT NULL,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`)
+);
---
+++ generate_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+start=5
+end=16
+
+for i in $(seq $start $end)
+do
+	touch "${i}.sql"
+	echo "-- File ${i}" > "${i}.sql"
+done
+
+echo "SQL files generated from ${start}.sql to ${end}.sql"
