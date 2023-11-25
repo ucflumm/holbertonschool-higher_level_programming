5c7f11d99abdb9d3f5d0b72c3e806ad5b84ee98d
Uwe Flumm <ucflumm@gmail.com>
2023-11-25 12:12:55 +1100

    Fix SQL query to select from correct table

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
-SELECT * FROM mysql.user WHERE user = 'user_0d_1' OR user = 'user_0d_2';
+SELECT * FROM user WHERE user = 'user_0d_1' OR user = 'user_0d_2';
