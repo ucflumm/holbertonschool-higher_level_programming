a0492e55cd40b2ad3759e167cb0df6feb4a70c3e
Uwe Flumm <ucflumm@gmail.com>
2023-11-25 12:15:19 +1100

    Update privileges SQL query

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
-SELECT * FROM user WHERE user = 'user_0d_1' OR user = 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
