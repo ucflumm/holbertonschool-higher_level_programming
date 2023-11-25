b6142d647499caa72c8480a84d0abedd157659b1
Uwe Flumm <ucflumm@gmail.com>
2023-11-25 13:48:57 +1100

    Add query to retrieve genre IDs by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -5,3 +5,7 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
