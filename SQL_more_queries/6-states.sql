4c74dcf4234deba0120e913dfb375c50bac6d291
Uwe Flumm <ucflumm@gmail.com>
2023-11-25 12:25:07 +1100

    Add states table to hbtn_0d_usa database

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -5,3 +5,11 @@
 -- name VARCHAR(256) can’t be null
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
