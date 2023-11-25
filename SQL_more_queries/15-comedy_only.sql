44ead7303aaf975ff8652b716bced18304889f72
Uwe Flumm <ucflumm@gmail.com>
2023-11-25 14:17:27 +1100

    Add query to retrieve comedy TV shows

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -6,3 +6,10 @@
 -- Results must be sorted in ascending order by the show title
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
