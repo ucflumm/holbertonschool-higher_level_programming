7daabed414e6718db2b7a95ab96fc0ac5ceae340
Uwe Flumm <ucflumm@gmail.com>
2023-11-25 14:14:24 +1100

    Add query to retrieve genres for TV show 'Dexter'

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -6,3 +6,10 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
+
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
