4714934fa8ae18b51d4c992828eb64a100e79bc9
Uwe Flumm <ucflumm@gmail.com>
2023-11-25 12:11:55 +1100

    Remove privileges SQL script

--- SQL_more_queries/0-priveleges.sql
+++
@@ -1,3 +0,0 @@
--- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-
-SELECT * FROM mysql.user WHERE user = 'user_0d_1' OR user = 'user_0d_2';
