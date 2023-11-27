1fc31d871bbbb9254d3ca97a1cd655c6b71fdcca
Uwe Flumm <ucflumm@gmail.com>
2023-11-27 23:18:42 +1100

    Add State class definition to model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+    Write a python file that contains the class definition of a State and an
+    instance Base = declarative_base()
+    inherits from Base
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated,
+    unique integer, cant be null and is a primary key
+    class attribute name that represents a column of a string with maximum 128
+    characters and cant be null
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        Class definition of a State and an instance Base = declarative_base()
+        inherits from Base
+        links to the MySQL table states
+        class attribute id that represents a column of an auto-generated,
+        unique integer, cant be null and is a primary key
+        class attribute name that represents a column of a string with maximum
+        128 characters and cant be null
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
