82f475fc851eab51ee0fca51071845098e3e9e42
Uwe Flumm <ucflumm@gmail.com>
2023-11-27 23:23:07 +1100

    Remove unnecessary class attributes in State model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -21,10 +21,6 @@ class State(Base):
         Class definition of a State and an instance Base = declarative_base()
         inherits from Base
         links to the MySQL table states
-        class attribute id that represents a column of an auto-generated,
-        unique integer, cant be null and is a primary key
-        class attribute name that represents a column of a string with maximum
-        128 characters and cant be null
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
