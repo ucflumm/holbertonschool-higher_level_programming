16ab513e9f848dafd1e6f8fc66fc304016336682
Uwe Flumm <ucflumm@gmail.com>
2023-11-27 23:36:53 +1100

    Add City model definition to model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+    a Python file similar to model_state.py named model_city.py that contains
+    the class definition of a City.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+        Class definition of a City and an instance Base = declarative_base()
+        inherits from Base
+        links to the MySQL table cities
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"))
+    state = relationship("State", back_populates="cities")
