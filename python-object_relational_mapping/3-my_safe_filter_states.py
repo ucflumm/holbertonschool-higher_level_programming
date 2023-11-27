ab7ab9fdb8e808334710e8953a843ccfe06ebaff
Uwe Flumm <ucflumm@gmail.com>
2023-11-27 23:04:52 +1100

    Add safe filtering for states table

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""
+    Once again, write a script that takes in arguments and displays all values
+    in the states table of hbtn_0e_0_usa where name matches the argument.
+    But this time, write one that is safe from MySQL injections!
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1:5]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s\
+                    ORDER BY id ASC", (state_name,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
